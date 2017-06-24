d461730fcf3732e926ffb5a882505911df383d80
miguelch96 <miguelangelca96@outlook.com>
2017-06-23 23:34:47 -0500

    Limpiar repo

--- newsic-spring/src/main/java/com/newsic/repository/ITipoUsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.newsic.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.newsic.entity.TipoUsuario;
-
-public interface ITipoUsuarioRepository extends CrudRepository<TipoUsuario,Long>{
-
-}
